8d5ccaddf8a299581e02141e553a40dbb78e11ac
adtyridho <144533561+adtyridho@users.noreply.github.com>
2024-07-17 18:08:30 +0700

    Update index.php
    hghgh

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION['username'])) {
             <a class="navbar-brand" href="#">
                 <img src="dist/img/library.png" alt="" width="50" height="40" />
             </a>
-            <a class="navbar-brand" href="#">BacaOnline</a>
+            <a class="navbar-brand" href="#">NovelOnline</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
